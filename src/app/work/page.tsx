f869ceb16e041c833c9b855101f42728f93550dd
Hec Sanchez <hola@hectorsanchez.mx>
2023-05-16 17:52:50 -0600

    responsive

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -19,27 +19,27 @@ export default function Home() {
                 <div className="mt-12">
                     <Wrapper >
                         <div className="relative z-10 max-w-4xl m-auto text-center">
-                            <p className="text-lg font-medium tracking-widest antialiased antialiased m-auto text-center">
+                            <p className="text-md sm:text-lg font-medium tracking-widest antialiased antialiased m-auto text-center">
                                 <span className="bg-text">MY WORK</span>
                             </p>
-                            <h1 className="text-6xl font-bold leading-snug antialiased inline block">
+                            <h1 className="text-3xl sm:text-6xl font-bold leading-snug antialiased inline block">
                                 Products I&apos;ve built
                             </h1>
-                            <p className="text-xl mt-3 max-w-[800px] m-auto antialiased leading-8">I ❤️ building software products, these are just some I&apos;ve built recently.</p>
+                            <p className="text-base sm:text-xl mt-3 max-w-[800px] m-auto antialiased leading-8">I ❤️ building software products, these are just some I&apos;ve built recently.</p>
 
                         </div>
                     </Wrapper>
                 </div>
-                <div className="mt-40 mb-40">
+                <div className="mt-10 mb-20 sm:mt-40 sm:mb-40">
                     <div className="flex flex-wrap max-w-[1200px] m-auto">
-                        <div className="flex mt-20">
-                            <div className="w-1/2 p-5">
+                        <div className="sm:flex mt-10 sm:mt-20">
+                            <div className="sm:w-1/2 p-5">
                                 <Link href="https://oppal.io" target="_blank">
                                     <Image alt="oppal logo" src={oppal} className="shadow-2xl shadow-slate-500/50 hover:scale-105 transition duration-300 cursor-pointer rounded-md" />
                                 </Link>
                             </div>
-                            <div className="w-1/2 p-5 text-lg">
-                                <h2 className="font-bold text-4xl mt-5 mb-8">Oppal</h2>
+                            <div className="sm:w-1/2 p-5 text-md sm:text-lg">
+                                <h2 className="font-bold text-2xl mt-2 mb-4 sm:text-4xl sm:mt-5 sm:mb-8">Oppal</h2>
                                 <p className="mb-4">This is a product I am growing to 10k dedicating 2 hours each day. I am recording the progress and struggles in <Link href="https://www.linkedin.com/newsletters/7045259033468956672/"><span className="underline">my newsletter</span></Link> (in Spanish).</p>
                                 <p className="mb-4">I went from idea to validation to having a fully functional product in 28 days.</p>
                                 <p>Used code to build the product, but used no-code to build the website to help validate the idea.</p>
@@ -52,9 +52,9 @@ export default function Home() {
                             </div>
                         </div>
 
-                        <div className="flex mt-20">
-                            <div className="w-1/2 p-5 text-lg">
-                                <h2 className="font-bold text-4xl mt-8 mb-8">Productosfera</h2>
+                        <div className="flex flex-col-reverse sm:flex-row mt-10 sm:mt-20">
+                            <div className="sm:w-1/2 p-5 text-md sm:text-lg">
+                                <h2 className="font-bold text-2xl mt-4 mb-4 sm:text-4xl sm:mt-8 sm:mb-8">Productosfera</h2>
                                 <p className="mb-4">A product/service discovery site that I helped build.</p>
                                 <p className="mb-4">With a JavaScript stack and a relational database we were able to build this product and launch it within 2 weeks of planning it.</p>
                                 <p>Used code to build the product, but used no-code to build the website to help validate the idea.</p>
@@ -64,25 +64,25 @@ export default function Home() {
                                     <Tag>PostgreSQL</Tag>
                                 </div>
                             </div>
-                            <div className="w-1/2 p-5">
+                            <div className="sm:w-1/2 p-5">
                                 <Link href="https://productosfera.com" target="_blank">
                                     <Image alt="productosfera logo" src={productosfera} className="shadow-2xl shadow-slate-500/50 hover:scale-105 transition duration-300 cursor-pointer rounded-md" />
                                 </Link>
                             </div>
                         </div>
 
-                        <div className="flex mt-20">
-                            <div className="w-1/2 p-5 ">
+                        <div className="mt-10 sm:flex sm:mt-20">
+                            <div className="sm:w-1/2 p-5 ">
                                 <Link href="https://soulfitness.mx" target="_blank">
                                     <Image alt="soul fitness logo" src={soulfitness} className="shadow-2xl shadow-slate-500/50 hover:scale-105 transition duration-300 cursor-pointer rounded-md"/>
                                 </Link>
                             </div>
-                            <div className="w-1/2 p-5 text-lg">
-                                <h2 className="font-bold text-4xl mt-8 mb-8">Soul Fitness</h2>
+                            <div className="sm:w-1/2 p-5 text-md sm:text-lg">
+                                <h2 className="font-bold text-2xl mt-4 mb-4 sm:text-4xl sm:mt-8 sm:mb-8">Soul Fitness</h2>
                                 <p className="mb-4">A system to manage and grow a fitness studio</p>
                                 <p className="mb-4">Soul Fitness founders were interested in having a system that could ease operations for their businesses but also that would help them scale, as they had big plans for their brand.</p>
                                 <p className="mb-4">Using web technologies I built a customer-facing bookings system as well as their internal dashboard to manage bookings, members, classes and instructors.</p>
-                                <div className="flex mt-10 gap-3">
+                                <div className="flex mt-10 gap-3 flex-wrap">
                                     <Tag>Next.js</Tag>
                                     <Tag>Node.js</Tag>
                                     <Tag>Tailwind</Tag>
@@ -91,13 +91,13 @@ export default function Home() {
                             </div>
                         </div>
 
-                        <div className="flex mt-20">
-                            <div className="w-1/2 p-5 text-lg">
-                                <h2 className="font-bold text-4xl mt-8 mb-8">Unitrabajos</h2>
+                        <div className="flex flex-col-reverse sm:flex-row sm: mt-20">
+                            <div className="sm:w-1/2 p-5 text-md sm:text-lg">
+                                <h2 className="font-bold text-2xl mt-4 mb-4 sm:text-4xl sm:mt-8 sm:mb-8">Unitrabajos</h2>
                                 <p className="mb-4">The job board for the new generations</p>
                                 <p className="mb-4">Unitrabajos is an effort to make job hunting easier for university students and recent graduates.</p>
                                 <p className="mb-4">It also aims to help companies recruit talent earlier on in their recruitment lifecycle and that way benefit both parties.</p>
-                                <div className="flex mt-10 gap-3">
+                                <div className="flex mt-10 gap-3 flex-wrap">
                                     <Tag>Next.js</Tag>
                                     <Tag>Node.js</Tag>
                                     <Tag>Tailwind</Tag>
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -5,7 +5,7 @@ interface TagProps {
 }
 export const Tag: FC<TagProps> = ({ children }) => {
     return (
-        <div className="border border-[#393446] py-2 px-6 rounded-lg  shadow-2xl shadow-slate-500/30 tracking-wide">
+        <div className="border border-[#393446]  px-3 py-2 sm:px-6 rounded-lg  shadow-2xl shadow-slate-500/30 tracking-wide text-sm sm:text-base">
             {children}
         </div>
     )
