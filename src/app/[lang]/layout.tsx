cdec20e722460a78c2fd63b8c52ccc4911cfd06b
Hec Sanchez <hola@hectorsanchez.mx>
2023-05-16 15:50:04 -0600

    Add GAnalytics

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,12 +1,12 @@
 import './globals.css'
 import { Poppins } from 'next/font/google'
-import {Header} from "@/components/Header";
+import Script from "next/script";
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'] })
 
 export const metadata = {
   title: 'Hec Sanchez - Product Engineer • JavaScript Developer',
-  description: 'Hec SancheZ is a Product Engineer with 15 years of experience building software with JavaScript',
+  description: 'Hec Sanchez is a Product Engineer with 15 years of experience building software with JavaScript',
 }
 
 export default function RootLayout({
@@ -21,6 +21,19 @@ export default function RootLayout({
           <div className="max-w-[1400px] m-auto py-10 flex justify-center">
               Hec Sanchez © 2018 - 2023 • All rights reserved
           </div>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=G-JNC5CX70DJ`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+            window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', 'G-JNC5CX70DJ');
+            `}
+            </Script>
       </body>
     </html>
   )
